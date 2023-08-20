e1b18cceeb5ab31bf094ee43de6c0b39d72dd8da
Hamid Shamsian <hamid64bits@gmail.com>
2023-08-20 19:28:31 +0400

    hw08/q1 added.

---
+++ Hamid_Shamsian_hw8_maktab_99/Q1/script.js
@@ -0,0 +1,41 @@
+const user = {};
+
+const setterGenerator = function (propertyKey) {
+   console.log(this); // will log 'user' object because 'this' is bound to 'user' in line 22.
+
+   return properyValue => {
+      this[propertyKey] = properyValue;
+      console.log(this); // again will log 'user' because arrow function does not rebound this keyword, but will constantly bind 'this' to 'outer context this' at its birthday!
+      return this;
+   };
+};
+
+const userNameSetter = setterGenerator.call(user, "name");
+console.log(userNameSetter("Jack")); // we can log it because 'this' is returned from the function.
+
+const userAgeSetter = setterGenerator.call(user, "age");
+console.log(userAgeSetter(30));
+
+console.log(user);
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+const privateObjectGenerator = () => {
+   const privateObject = {};
+   return {
+      setProperty(key, value) {
+         privateObject[key] = value;
+      },
+      get() {
+         return privateObject;
+      }
+   };
+};
+
+const myPrivateObject = privateObjectGenerator();
+
+// console.log(myPrivateObject.get());
+
+myPrivateObject.setProperty("name", "alex");
+
+// console.log(myPrivateObject.get());
