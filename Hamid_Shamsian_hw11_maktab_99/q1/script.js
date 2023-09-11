012b3031dffbcde42d708b83e5c52b7d04b9d25e
Hamid Shamsian <hamid64bits@gmail.com>
2023-09-11 17:05:41 +0400

    hw11/q1: derived polygon class implemented.

--- Hamid_Shamsian_hw11_maktab_99/q1/script.js
+++ Hamid_Shamsian_hw11_maktab_99/q1/script.js
@@ -25,3 +25,33 @@ class Shape {
     throw new Error("Method of Abstract Class has no implementation. Implement this method in the derived Class in order to use that.");
   }
 }
+
+// derived classes:
+
+class Polygon extends Shape {
+  #sidesCount;
+  #oneSideLength;
+
+  constructor(shapeName, sidesCount, oneSideLength) {
+    super(shapeName);
+    this.#sidesCount = sidesCount;
+    this.#oneSideLength = oneSideLength;
+  }
+
+  calcPerimeter() {
+    return this.#sidesCount * this.#oneSideLength;
+  }
+
+  calcArea() {
+    return (this.calcPerimeter() * this.#oneSideLength) / (Math.tan(Math.PI / this.#sidesCount) * 4);
+  }
+}
+
+const polygon = new Polygon("my polygon", 6, 10);
+
+console.log(polygon.calcPerimeter());
+console.log(polygon.calcArea());
+
+console.log(polygon.shapeName);
+polygon.shapeName = "his polygon";
+console.log(polygon.shapeName);
