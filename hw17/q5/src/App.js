8cafe6f6534b35e0ab752c379996f4812d5ccbdb
Hamid Shamsian <hamid64bits@gmail.com>
2023-10-19 03:53:26 +0400

    hw17/q5 added.

---
+++ hw17/q5/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HelloWorld from "./HelloWorld";
+
+const App = () => {
+  return (
+    <div className='app'>
+      <HelloWorld />
+    </div>
+  );
+};
+
+export default App;
