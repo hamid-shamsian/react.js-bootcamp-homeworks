e3f87d2d9e71d0c94691d5e3b6a08bdb58f17ee4
Hamid Shamsian <hamid64bits@gmail.com>
2023-10-26 00:31:35 +0400

    hw18: feat: App component initiated.

---
+++ hw18/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import MovieForm from "./components/MovieForm";
+import Movie from "./components/Movie";
+import data from "./data.json";
+
+const App = () => {
+  const [movies, setMovies] = useState(data.movies);
+
+  const handleSubmitMovie = movie => {
+    setMovies([...movies, movie]);
+  };
+
+  return (
+    <div className='container mx-auto flex flex-col items-center p-6 pb-10 max-w-xl'>
+      <MovieForm onSubmitMovie={handleSubmitMovie} />
+
+      <div className='flex flex-col gap-10 mt-10'>
+        {movies.map(m => (
+          <Movie key={m.id} movie={m} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
