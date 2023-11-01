003b47063e97703d88d3029710363892019c14ce
Hamid Shamsian <hamid64bits@gmail.com>
2023-11-01 17:47:38 +0400

    hw18: feat: App component +page and notes states.

--- hw18/src/App.jsx
+++ hw18/src/App.jsx
@@ -1,3 +1,15 @@
-const App = () => {};
+import { useEffect, useState } from "react";
+import Notes from "./components/pages/Notes";
+
+const App = () => {
+  const [page, setPage] = useState("notes");
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    setNotes(JSON.parse(localStorage.getItem("notes")));
+  }, []);
+
+  return <div className='bg-dark text-white h-screen px-5 pt-28 font-nunito'>{page === "notes" && <Notes notes={notes} />}</div>;
+};
 
 export default App;
