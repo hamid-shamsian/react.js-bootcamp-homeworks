b0e2d73b62842e5fba01cf498ac1df6b22ed72aa
Hamid Shamsian <hamid64bits@gmail.com>
2023-11-08 20:13:00 +0400

    hw19: feat: Card component.

---
+++ hw19/src/components/base/card/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  box-shadow: 0px 4px 24px 0px rgba(123, 123, 123, 0.15);
+  font-family: Poppins;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0px 4px 24px 0px rgba(123, 123, 123, 0.35);
+  }
+`;
