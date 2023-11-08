227ebf3950169b6996cd93e6e2ac348fda30ce25
Hamid Shamsian <hamid64bits@gmail.com>
2023-11-09 00:54:10 +0400

    hw19: feat: Button component improved.

--- hw19/src/components/base/button/index.jsx
+++ hw19/src/components/base/button/index.jsx
@@ -4,8 +4,11 @@ export const Button = styled.button`
   border-radius: 4px;
   background: #56b280;
   border: 0;
-  padding: 7px 50px;
+  padding: 10px 50px;
   color: #fff;
   font-family: Roboto;
   font-size: 20px;
+  display: flex;
+  justify-content: center;
+  gap: 10px;
 `;
