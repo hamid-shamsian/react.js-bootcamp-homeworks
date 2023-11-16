e915eb93448d25a6b2d5d33cc4712205b4b1c3ca
Hamid Shamsian <hamid64bits@gmail.com>
2023-11-16 20:08:20 +0330

    hw20/q2: feat: weatherCards and some styling... +functionality improved.

--- hw20/q2/src/App.jsx
+++ hw20/q2/src/App.jsx
@@ -1,54 +1,63 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import Input from "./components/Input";
+import Weather from "./components/Weather";
 import { debounce } from "./utils/utilityFuncs";
 import { api_key, geo_url, wth_url } from "./config";
-import Weather from "./components/Weather";
 
 const App = () => {
   const [cityQuery, setCityQuery] = useState("");
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const fetchWeather = useCallback(
-    debounce(async city => {
-      try {
-        const { data: cityCoord } = await axios.get(`${geo_url}?q=${city}&limit=1${api_key}`);
-        if (cityCoord[0]) {
-          const { lat, lon } = cityCoord[0];
-          const weather = await axios.get(`${wth_url}?lat=${lat}&lon=${lon}${api_key}`);
-          setWeather(weather.data);
+  // I changed 👇 useCallback to useMemo because of a weird warning about useCallback saying: "React Hook useCallback received a function whose dependencies are unknown"! and after searching the issue in stackoverflow, I realized that useCallback and useMemo hooks expect inline functions as the first parameter, so i changed useCallback to useMemo to be able to return that debounced function in the body of an inline arrow function passed to useMemo! :)
+  const fetchWeather = useMemo(
+    () =>
+      debounce(async city => {
+        if (!city) return setWeather(null);
+        try {
           setError(false);
-        } else {
-          setWeather(null);
+          const { data: cityCoord } = await axios.get(`${geo_url}?q=${city}&limit=1${api_key}`);
+
+          if (cityCoord[0]) {
+            const { lat, lon } = cityCoord[0];
+            const weather = await axios.get(`${wth_url}?lat=${lat}&lon=${lon}${api_key}`);
+            setWeather(weather.data);
+          } else {
+            setWeather(null);
+          }
+        } catch (error) {
+          setError(true);
+        } finally {
+          setLoading(false);
         }
-      } catch (error) {
-        setError(true);
-      }
-    }, 700),
+      }, 500),
     []
   );
 
   useEffect(() => {
-    if (cityQuery.length) fetchWeather(cityQuery);
-    else setWeather(null);
+    setLoading(true);
+    setWeather(null);
+    fetchWeather(cityQuery);
   }, [cityQuery, fetchWeather]);
 
   const onInputChange = ({ target }) => setCityQuery(target.value);
 
   return (
-    <div className='bg-gray-700 h-screen pt-10'>
+    <div className='bg-gray-700 min-h-screen pt-10'>
       <Input value={cityQuery} onChange={onInputChange} />
 
-      <main className='bg-gray-900 w-[600px] mx-auto rounded-xl text-white p-7'>
-        {weather && <Weather weather={weather} />}
+      {cityQuery && (
+        <main className='bg-gray-900 w-[700px] mx-auto rounded-xl text-white p-10 mt-16'>
+          {weather && <Weather weather={weather} />}
 
-        {cityQuery && !weather && <p className='text-center'>City Not Found!</p>}
+          {cityQuery && !weather && !loading && <p className='text-center'>City Not Found!</p>}
+          {cityQuery && !weather && loading && <p className='text-center'>Searching!</p>}
 
-        {!cityQuery && <p className='text-center'>Search for a City...</p>}
-
-        {error && <p className='text-center'>Something went wrong!</p>}
-      </main>
+          {error && <p className='text-center'>Something went wrong!</p>}
+        </main>
+      )}
     </div>
   );
 };
